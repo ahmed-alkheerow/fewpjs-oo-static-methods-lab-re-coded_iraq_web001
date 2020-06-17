bdd9dafc24630bf098cf2aece46974bec6042892
Ahmed Al-kheerow <a.alkheerow@gmail.com>
2020-06-17 15:12:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 class Formatter {
   //add static methods here
+  static capitalize (string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string){
+     return string.replace(/[^A-Za-z0-9-']+/g, '');
+  }
+  static titleize (string){
+    const  noEffect = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    let newString = string.split('').map(word => {
+      noeffect.includes(word) ? word:capitalize(word)).join('')
+    })
+    return  this.capitalize(newString);
+  }
 }
